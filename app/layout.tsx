0dba3eed4f458b8ed6e0abf3ea74a3341ff5abd4
israel <israeliyanda5@gmail.com>
2024-11-16 15:01:12 +0100

    fix theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export const viewport: Viewport = {
   initialScale: 1.0,
   maximumScale: 1.0,
   userScalable: false,
-  themeColor: "#1181ED",
+  themeColor: "#ffffff",
 };
 
 export default function RootLayout({
